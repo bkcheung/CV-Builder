2cb28dc362ddce40e645caaaeeebef2eea9ba0d7
bkcheung <b.cheung@hotmail.com>
2024-08-18 22:56:56 -0400

    feat: add default experience data

--- src/data.ts
+++ src/data.ts
@@ -2,6 +2,8 @@ import { v4 as uuid } from 'uuid';
 
 export type pInfoType = 'name'|'email'|'number'|'address';
 export type eduType = 'school'|'degree'|'start'|'end'|'location'|'id';
+export type expType = 'company'|'title'|'description'|'start'|'end'|'location'|'id';
+
 
 export const defPInfo:Record<pInfoType,string> = {
     name: 'Bonnie Cheung',
@@ -15,5 +17,17 @@ export const defEducations:Record<eduType,string>[] = [{
     start: '2014-09',
     end: '2019-05',
     location: 'Waterloo, Ontario',
+    id: uuid(),
+}]
+export const defExp:Record<expType,string>[] = [{
+    company: 'Apple',
+    title: 'iPad Product Design Engineer',
+    description:'·Reduced R&D cost by ~$2M by architecting a unified MLB design that can be shared between different sized products\n\
+                 ·Collaborated with cross-functional teams, including EE, Thermals, Antenna/RF, Rel, and factory teams to design a\
+                  variety of flexes and PCBs for next generation products.\n\
+                 ·Spearheaded innovative new design for speaker mesh to reduce liquid ingress',
+    start: '2022-01',
+    end: '2023-06',
+    location: 'Cupertino, California',
     id: uuid(),
 }]
