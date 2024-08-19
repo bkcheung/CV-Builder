6431c442d4e66f997f41e3766552d72fa98bef31
bkcheung <b.cheung@hotmail.com>
2024-08-19 12:00:11 -0400

    refactor: update using tailwind

--- src/CV.tsx
+++ src/CV.tsx
@@ -24,15 +24,16 @@ export default function CV({ pInfo, eduInfo, expInfo }:cvProps): JSX.Element {
         <div className="flexRow">
           <div className="cvContact">
             <img src="/mail.png" style={{ height: "1em" }}></img>
-            <div style={{ marginLeft: 5 }}>{pInfo.email}</div>
+            {/* <div style={{ marginLeft: 5 }}>{pInfo.email}</div> */}
+            <div className="ml-1">{pInfo.email}</div>
           </div>
           <div className="cvContact">
             <img src="/telephone.png" style={{ height: "1em" }}></img>
-            <div style={{ marginLeft: 5 }}>{pInfo.number}</div>
+            <div className="ml-1">{pInfo.number}</div>
           </div>
           <div className="cvContact">
             <img src="/location.png" style={{ height: "1em" }}></img>
-            <div style={{ marginLeft: 5 }}>{pInfo.address}</div>
+            <div className="ml-1">{pInfo.address}</div>
           </div>
         </div>
       </div>
