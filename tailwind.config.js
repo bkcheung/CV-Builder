18cc789dffa07573c634380f5648c2d327a4063a
bkcheung <b.cheung@hotmail.com>
2024-08-22 13:23:17 -0400

    env: update dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-export default{
+export default {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
     "./index.html",
-],
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
